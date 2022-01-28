68653205d6d1c6c9cf8807ab6d6cc5f734d7776e
Rodolfo Matos <rodolfo.matos@forto.com>
2022-01-28 17:02:34 +0100

    add comment deduplication feature
    This feature will allow to create a comment and perform a deduplication
    of previous comments that match a deduplication key

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function run(): Promise<void> {
   try {
     const githubToken = core.getInput('github_token', { required: true });
     const body = core.getInput('body', { required: true });
+    const deduplicate = core.getInput('deduplicate', { required: false });
 
     const octokit = github.getOctokit(githubToken);
 
@@ -17,6 +18,23 @@ async function run(): Promise<void> {
       core.getInput('number') === ''
         ? github.context.issue.number
         : parseInt(core.getInput('number'));
+
+    if (deduplicate) {
+      const comments = await octokit.issues.listComments({
+        owner,
+        repo,
+        issue_number: number
+      })
+      const comment_ids = comments.filter(comment => comment.body.includes(deduplicate))
+      for (const comment_id of comment_ids) {
+        await octokit.issues.deleteComment({
+          owner,
+          repo,
+          issue_number: number,
+          comment_id,
+        })
+      }
+    }
 
     await octokit.issues.createComment({
       owner,
